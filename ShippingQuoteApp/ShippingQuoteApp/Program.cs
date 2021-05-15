f33414e30c48643c59920ce7c2002b3b6b620501
SamLehner <samblehner8@gmail.com>
2021-05-14 20:27:32 -0600

    Fixed Shipping App Code
    Fixed bugs in code

--- ShippingQuoteApp/ShippingQuoteApp/Program.cs
+++ ShippingQuoteApp/ShippingQuoteApp/Program.cs
@@ -19,35 +19,39 @@ namespace ShippingQuoteApp
             if (packageWeightInt > 50)
             {
                 Console.WriteLine("You're package is too heavy to be shipped via Package Express. Have a good day!");
-            }
-            Console.ReadLine();
-
-            Console.WriteLine("What is the width of the package you would like shipped?");
-            string packageWidth = Console.ReadLine();
-            int packageWidthInt = Convert.ToInt32(packageWidth);
-
-            Console.WriteLine("What is the height of the package you would like shipped?");
-            string packageHeight = Console.ReadLine();
-            int packageHeightInt = Convert.ToInt32(packageHeight);
-
-            Console.WriteLine("What is the length of the package you would like shipped?");
-            string packageLength = Console.ReadLine();
-            int packageLengthInt = Convert.ToInt32(packageLength);
-
-            if (packageWidthInt + packageHeightInt + packageLengthInt > 50)
-            {
-                Console.WriteLine("Your package is too big to be shipped!");
+                Console.ReadLine();
             }
             else
             {
-                Console.WriteLine("Your package is within the limits to be shipped!");
-                int quote = ((packageHeightInt * packageLengthInt * packageWeightInt) / 100);
-                Console.WriteLine("Your estimated total for shipping is: " + quote);
-                Console.ReadLine();
-            }
-            Console.ReadLine();
 
-            
+
+
+
+                Console.WriteLine("What is the width of the package you would like shipped?");
+                string packageWidth = Console.ReadLine();
+                int packageWidthInt = Convert.ToInt32(packageWidth);
+
+                Console.WriteLine("What is the height of the package you would like shipped?");
+                string packageHeight = Console.ReadLine();
+                int packageHeightInt = Convert.ToInt32(packageHeight);
+
+                Console.WriteLine("What is the length of the package you would like shipped?");
+                string packageLength = Console.ReadLine();
+                int packageLengthInt = Convert.ToInt32(packageLength);
+
+                if (packageWidthInt + packageHeightInt + packageLengthInt > 50)
+                {
+                    Console.WriteLine("Your package is too big to be shipped!");
+                }
+                else
+                {
+                    Console.WriteLine("Your package is within the limits to be shipped!");
+                    int quote = ((packageHeightInt * packageLengthInt * packageWidthInt) / 100);
+                    Console.WriteLine("Your estimated total for shipping is: " + quote);
+                }
+                Console.ReadLine();
+
+            }
 
 
                 
