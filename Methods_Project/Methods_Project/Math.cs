06302d903809266c4f61964e9cf6548f5fa7f2c2
SamLehner <samblehner8@gmail.com>
2021-05-21 17:30:07 -0600

    Method Project
    A project making classes and methods in C#

---
+++ Methods_Project/Methods_Project/Math.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Project
+{
+    public class Math
+    {
+        public int Addition(int a)
+        {
+            return a + 25;
+        }
+
+        public int Multiply(int a)
+        {
+            return a * 25;
+        }
+
+        public int Division(int a)
+        {
+            return a / 25;
+        }
+
+    }
+}
---
+++ Methods_Project/Methods_Project/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an instance of the class for later
+            Math math = new Math();
+            // Asking the user for an input
+            Console.WriteLine("What number do you want changed by 25? ");
+            int answer = Convert.ToInt32(Console.ReadLine());
+
+            // Calling each method and passing the variable the user gives us to do math opertaions.
+            Console.WriteLine(math.Addition(answer));
+            Console.WriteLine(math.Multiply(answer));
+            Console.WriteLine(math.Division(answer));
+            Console.ReadLine();
+        }
+    }
+}
