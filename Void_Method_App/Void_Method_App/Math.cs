4557e260db7045abdad67b2436c02bb35f4b959a
SamLehner <samblehner8@gmail.com>
2021-05-24 21:11:34 -0600

    Void Method App
    An app using a void method and specific parameter names

---
+++ Void_Method_App/Void_Method_App/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method_App
+{
+    class Math
+    {
+        public void Addition(int a, int b)
+        {
+            int total = a * 2;
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ Void_Method_App/Void_Method_App/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instaniating the method
+            Math math = new Math();
+
+            // Getting user inputs and converting to int
+            Console.WriteLine("Please enter a number");
+            int answer = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter another number");
+            int answer2 = Convert.ToInt32(Console.ReadLine());
+
+
+            // Calling our method using parameter names
+            math.Addition(a: answer, b: answer2);
+            Console.ReadLine();
+        }
+    }
+}
