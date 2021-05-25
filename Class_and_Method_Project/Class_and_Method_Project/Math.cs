e44056f13171907c1d1ff566dbf5b63ee39886d1
SamLehner <samblehner8@gmail.com>
2021-05-24 21:41:45 -0600

    Class and Methods project
    A project using static classes

---
+++ Class_and_Method_Project/Class_and_Method_Project/Math.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_and_Method_Project
+{
+    static class Math
+    {
+        public static void Division(int a)
+        {
+            int total = a / 2;
+            Console.WriteLine(total);
+        }
+
+        public static void Division(int a, int b, out int c)
+        {
+            c = a / b;
+            
+        }
+    }
+}
---
+++ Class_and_Method_Project/Class_and_Method_Project/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_and_Method_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Math math = new Math();
+            int answer4 = 0;
+            
+            Console.WriteLine("Please enter a number");
+            int answer = Convert.ToInt32(Console.ReadLine());
+
+            Math.Division(a: answer);
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter one number");
+            int answer2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter another number");
+            int answer3 = Convert.ToInt32(Console.ReadLine());
+
+            Math.Division(a: answer2, b: answer3, c: out answer4);
+            Console.WriteLine(answer4);
+            Console.ReadLine();
+
+        }
+    }
+}
