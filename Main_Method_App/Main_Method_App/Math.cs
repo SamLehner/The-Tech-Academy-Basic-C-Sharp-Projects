b4bea7891a340cc9574dbe65bd91648bbf9454fb
SamLehner <samblehner8@gmail.com>
2021-05-24 20:43:57 -0600

    Main Method App
    An app using method overloading.

---
+++ Main_Method_App/Main_Method_App/Math.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_App
+{
+    public class Math
+    {
+        /// <summary>
+        /// Method for taking an integer value and adding 30 to it
+        /// </summary>
+        /// <param name="a">User input int</param>
+        /// <returns>int value</returns>
+        public int Addition(int a)
+        {
+            int total = a + 30;
+            return total;
+        }
+        // Same as the top one but using decimal
+        public int Addition(decimal b)
+        {
+            decimal total = b + 25;
+            int convertedTotal = Convert.ToInt32(total);
+            return convertedTotal;
+        }
+        // Same as before but using a string
+        public int Addition(string c)
+        {
+            
+            int total = Convert.ToInt32(c) + 20;
+            return total;
+        }
+    }
+}
---
+++ Main_Method_App/Main_Method_App/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating the class of math in the main program
+            Math math = new Math();
+
+            //Asking for user input to do opertioans on
+            Console.WriteLine("What is your favorite number?");
+            int answer = Convert.ToInt32(Console.ReadLine());
+
+            // Calling the method Addition using an int value
+            Console.WriteLine(math.Addition(answer));
+            Console.ReadLine();
+
+            // Asking user for decimal value
+            Console.WriteLine("What is your favorite decimal number?");
+            decimal deciAnswer = Convert.ToDecimal(Console.ReadLine());
+
+            // Calling a mehtod with the same name but passing in a decimal
+            Console.WriteLine(math.Addition(deciAnswer));
+            Console.ReadLine();
+
+            Console.WriteLine("What is your favorite number?");
+            string stringAnswer = Console.ReadLine();
+
+            // Calling a method with the same name but passing in a string value.
+            Console.WriteLine(math.Addition(stringAnswer));
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
