3e92d688d487311ded125052b0cd00e5a3482dda
SamLehner <samblehner8@gmail.com>
2021-05-26 00:27:43 -0600

    Parsing enums Project
    A project parsing enums we created

---
+++ Parsing_Enums_Project/Parsing_Enums_Project/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Puting code into a try block to catch user input errors
+            try
+            {
+                // Asking the user to enter the day of the week
+                    Console.WriteLine("Please enter the day of the week! ");
+                string answer = Console.ReadLine();
+
+                // Parsing the enum to convert our user entered string to our enum values
+                Weekday answer1 = (Weekday)Enum.Parse(typeof(Weekday), answer);
+                Console.WriteLine(answer1);
+            }
+            // If user doesnt enter an enum it will send this meesage
+            catch
+            {
+                Console.WriteLine("Please enter a day of the week! ");
+            }
+            
+
+            
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Parsing_Enums_Project/Parsing_Enums_Project/Weekdays.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums_Project
+{
+    public class Weekdays
+    {
+        // Out property of Weekdays
+        public Weekday Weekday { get; set; }
+    }
+
+    // Our enum for weekdays
+    public enum Weekday
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
