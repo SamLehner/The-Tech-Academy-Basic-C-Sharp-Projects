9aaed73d27a71794b49a7f8684da41fa8ef66540
SamLehner <samblehner8@gmail.com>
2021-05-28 14:48:12 -0600

    DateTime Project
    Project using DateTime

---
+++ DateTime Project/DateTime Project/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("What is your favorite number?");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(DateTime.Now.AddHours(answer));
+            Console.ReadLine();
+        }
+    }
+}
