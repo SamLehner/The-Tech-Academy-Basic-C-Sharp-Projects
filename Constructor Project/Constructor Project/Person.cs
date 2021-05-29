6e2b798e712cc53d9b50742366e7c51a017c9de9
SamLehner <samblehner8@gmail.com>
2021-05-28 19:08:11 -0600

    Constructor Project
    Constructor Project

---
+++ Constructor Project/Constructor Project/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Project
+{
+    public class Person
+    {
+        public Person(string name) : this (name, " Okay" )
+        {
+
+        }
+        public Person(string name, string wellness)
+        {
+            Name = name;
+            Wellness = wellness;
+
+        }
+
+        public string Name { get; set; }
+        public string Wellness { get; set; }
+    }
+}
---
+++ Constructor Project/Constructor Project/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating a constant
+            const string proName = "Bass Pro Shop! ";
+            Console.WriteLine("Welcome to {0}", proName);
+
+            // Using variables using var
+            var howAreYou = "How are you?";
+            Console.WriteLine(howAreYou);
+            string answer = Console.ReadLine();
+            var name = "What is your name?";
+            Console.WriteLine(name);
+            string answer2 = Console.ReadLine();
+
+            // Creating a person object and passing in the name they give, if no wellness check is types the default will appear
+            Person person = new Person(answer2);
+            Console.WriteLine(person.Name + person.Wellness);
+
+            Console.ReadLine();
+
+        }
+    }
+}
