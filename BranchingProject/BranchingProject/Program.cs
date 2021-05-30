aac05fef824ec19978d2a610bfe2a8de5adeec89
SamLehner <samblehner8@gmail.com>
2021-05-29 19:24:34 -0600

    Random Code
    Random Code

---
+++ BranchingProject/BranchingProject/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingProject
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("What is your favorite number?");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            string result = favNum == 12 ? "You have an awesome number" : "You dont have an awesome number.";
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            //int roomTemperature = 70;
+
+            //Console.WriteLine("Hi what is your name?");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi, " + name + ", what is the temperature where you are?");
+            //int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature")
+            //}
+            
+
+            //int currentTemperature = 70;
+            //int roomTemperature = 70;
+
+            //string comparisonResult = currentTemperature == roomTemperature ? "It is room temp." : "It is not room temp";
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature!");
+            //}
+            //else if (currentTemperature > roomTemperature) ;
+            //{
+            //    Console.WriteLine("It is warmer than room temperature")
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It is not exactly room temperature.");
+            //}
+            Console.ReadLine();
+        }
+    }
+}
--- Constructor Project/Constructor Project/Person.cs
+++ Constructor Project/Constructor Project/Person.cs
@@ -8,17 +8,20 @@ namespace Constructor_Project
 {
     public class Person
     {
+        // Overriding our constructor so wellness will have a value of OKay if left blank
         public Person(string name) : this (name, " Okay" )
         {
 
         }
+
+        // Our person constructor
         public Person(string name, string wellness)
         {
             Name = name;
             Wellness = wellness;
 
         }
-
+        // Our person properties
         public string Name { get; set; }
         public string Wellness { get; set; }
     }
